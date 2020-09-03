7d3c8844061ac5add72b4e8c749d6e95ff95203e
Hank <Hank@macdeMacBook-Air-2.local>
2020-09-04 00:29:18 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd public
+
+git init
+git remote add origin https://github.com/ihanking/ihanking.github.io.git
+git add .
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f origin master
+
+cd -
